3bb0756d76e8863c5b4f113782434b84ed502dbc
Lily Li <ylilyli24@gmail.com>
2016-10-23 15:49:43 -0700

    OLS: Regression

---
+++ code/scripts/regressions/OLS.R
@@ -0,0 +1,10 @@
+
+# import data
+credit = read.csv("../../../data/scaled-credit.csv")
+
+# OLS regression
+OLS_reg = lm(Balance ~., data = credit)
+
+# table summary
+library(pander)
+pander(OLS_reg)
