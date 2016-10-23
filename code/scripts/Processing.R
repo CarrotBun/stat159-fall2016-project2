34e9c878f9fc72bd642cf0f9f79eb9f57d751121
Lily Li <ylilyli24@gmail.com>
2016-10-23 14:54:28 -0700

    Data Processing: scaled variables

--- code/scripts/Processing.R
+++ code/scripts/Processing.R
@@ -1,1 +1,13 @@
-credits_data = read.csv("../data/Credit.csv")
+credit_df <- read.csv("../../data/Credit.csv")
+
+# dummy out categorical variables 
+temp_credit <- model.matrix(Balance ~ ., data = credit_df)
+
+# removing column of ones, and appending regressors to Balance
+new_credit_df <- cbind(temp_credit[ ,-1], Balance = credit_df$Balance)
+
+# scaling and centering
+scaled_credit <- scale(new_credit_df, center = TRUE, scale = TRUE)
+
+# export scaled data
+write.csv(scaled_credit, file = "../../data/scaled-credit.csv")
