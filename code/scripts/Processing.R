5084ca5a42bcc4c4a0dc730b233322b020e8a72b
Lily Li <ylilyli24@gmail.com>
2016-10-23 15:41:28 -0700

    Data Processing: removed colnames from scaled data

--- code/scripts/Processing.R
+++ code/scripts/Processing.R
@@ -1,4 +1,4 @@
-credit_df <- read.csv("../../data/Credit.csv")
+credit_df <- read.csv("../../data/Credit.csv", row.names = NULL)
 
 # dummy out categorical variables 
 temp_credit <- model.matrix(Balance ~ ., data = credit_df)
@@ -8,6 +8,8 @@ new_credit_df <- cbind(temp_credit[ ,-1], Balance = credit_df$Balance)
 
 # scaling and centering
 scaled_credit <- scale(new_credit_df, center = TRUE, scale = TRUE)
+scaled_credit <- scaled_credit[ ,-1]
+
 
 # export scaled data
-write.csv(scaled_credit, file = "../../data/scaled-credit.csv")
+write.csv(scaled_credit, file = "../../data/scaled-credit.csv", row.names = FALSE)
