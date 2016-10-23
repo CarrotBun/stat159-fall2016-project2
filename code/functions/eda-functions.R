82e82eee58a9276fad00624d8dca9fc7d8f86817
Elly Wang <ellywjy@berkeley.edu>
2016-10-23 16:41:05 -0700

    eda: output summary for quantitative variables

---
+++ code/functions/eda-functions.R
@@ -0,0 +1,27 @@
+# getSummary returns the stats needed for eda
+getSummary <- function(data){
+	vec <- c(summary(data),
+		round(IQR(data),2),
+		round(range(data)[2]-range(data)[1],2), 
+		round(sd(data),2))
+
+	names(vec) <- c( "Min.","1st Qu.","Median", 
+		"Mean","3rd Qu.","Max.", "IQR", "Range","SD")
+
+	vec
+}
+
+getFreq <- function(data){
+	freqList <- apply(data, 2, table)
+	organize_freq(freqList, nrow(data))
+	
+}
+
+organize_freq <- function(freqList, total){
+	for (i in 1:length(freqList)){
+		print(names(freqList)[i])
+		#print(head(freqList[[i]]))
+		print(rbind(freqList[[i]], freqList[[i]]/total))
+	}
+
+}
---
+++ code/scripts/eda-script.R
@@ -0,0 +1,46 @@
+library(ggplot2)
+library(dplyr)
+
+
+source("code/functions/eda-functions.R")
+credit <- read.csv("data/Credit.csv")
+
+quant_credit <- credit[,c(2:7,12)]
+cat_credit <- credit[,c(8:11)]
+
+
+
+
+corr_matrix <- round(quant_credit,4)
+lower <- corr_matrix
+lower[lower.tri(corr_matrix, diag =FALSE)] <- ""
+lower <- data.frame(lower)
+
+
+
+sink("data/eda-output.txt")
+
+cat("Summary of Quantiative Variables in Credit:\n")
+t(apply(quant_credit,2,getSummary))
+
+cat("\n Correlation Matrix:\n")
+lower
+sink()
+
+ggplot(ad_data) + geom_histogram(aes(TV))
+ggsave("images/histogram-tv.png")
+
+ggplot(ad_data) + geom_histogram(aes(Radio))
+ggsave("images/histogram-radio.png")
+
+ggplot(ad_data) + geom_histogram(aes(Newspaper))
+ggsave("images/histogram-newspaper.png")
+
+ggplot(ad_data) + geom_histogram(aes(Sales))
+ggsave("images/histogram-sales.png")
+
+png(filename="images/scatterplot-matrix.png")
+pairs(ad_data[-1],pch=18)
+dev.off()
+
+save(lower, file = "data/correlation-matrix.RData")
