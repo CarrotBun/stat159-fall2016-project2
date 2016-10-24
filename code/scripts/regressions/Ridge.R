b9f45e6390b7b38970af9f3994144624bf329846
Elly Wang <ellywjy@berkeley.edu>
2016-10-23 23:01:59 -0700

    ridge: added comments to script, fixed formatting

--- code/scripts/regressions/Ridge.R
+++ code/scripts/regressions/Ridge.R
@@ -1,5 +1,6 @@
 library(glmnet)
 
+# Load Data and T-T indicies
 source("code/scripts/Train-Test.R")
 
 credit = read.csv("data/scaled-credit.csv")
@@ -10,23 +11,27 @@ y.test = y[test_set_indices]
 
 grid = 10^seq(10, -2, length = 100)
 
-
+# Run ridge regression on training set
 ridge_reg = glmnet(x[train_set_indices,],y[train_set_indices], 
 	alpha = 0,lambda = grid)
+
+# Cross Validation, set seed to make work reproducible
 set.seed(159)
 ridge_cv = cv.glmnet(x[train_set_indices ,],y[train_set_indices], alpha =0, nfold = 10, lambda = grid, intercept =FALSE,standardize = FALSE)
-bestlam=ridge_cv$lambda.min
-bestlam
 
+# Best lambda/model
+bestlam=ridge_cv$lambda.min
+
+# Validation plot
 png(filename="images/ridge-validation.png")
 plot(ridge_cv)
 dev.off()
 
+# Apply best model to test set
+ridge_pred = predict(ridge_reg,s=bestlam,newx=x[test_set_indices,])
+ridge_tMSE <- mean((ridge_pred-y.test)^2)
 
-ridge_pred=predict(ridge_reg,s=bestlam,newx=x[test_set_indices,])
-testMSE <- mean((ridge_pred-y.test)^2)
-
-
+#Full Model
 out=glmnet(x,y,alpha=0)
 ridge_final <- predict(out,type="coefficients",s=bestlam)
 
@@ -36,12 +41,12 @@ sink("data/ridge-results.txt")
 
 cat("Best Lambda:\n")
 bestlam
-cat("Official Coefficients:\n")
-ridge_final
-cat("Test MSE:\n")
+cat("\n Test MSE:\n")
 testMSE
+cat("\n Official Coefficients:\n")
+ridge_final
 
 sink()
 
-save(ridge_pred, ridge_cv, bestlam,testMSE,ridge_final, file ="data/Ridge-Regression.RData")
+save(ridge_cv, bestlam,ridge_tMSE,ridge_final, file ="data/Ridge-Regression.RData")
 
