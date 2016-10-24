fe3ce43d2deab6e7e5179a5ab013d9ea1f864bd6
Elly Wang <ellywjy@berkeley.edu>
2016-10-23 22:47:21 -0700

    ridge: fixed error in code, produced outputs and images

--- code/scripts/regressions/Ridge.R
+++ code/scripts/regressions/Ridge.R
@@ -1,39 +1,47 @@
 library(glmnet)
 
-credit = read.csv("data/scaled-credit.csv")
-
-x = model.matrix(Balance ~ .,data = credit)[,-1] 
-y = credit$Balance
-
-grid = 10^seq(10, -2, length = 100)
-ridge.mod = glmnet(x, y, alpha = 0, lambda = grid)
-
 source("code/scripts/Train-Test.R")
 
+credit = read.csv("data/scaled-credit.csv")
+
+x = model.matrix(Balance ~ .,data = credit) 
+y = credit$Balance
 y.test = y[test_set_indices]
 
-
-ridge.mod = glmnet(x[train_set_indices,],y[train_set_indices], alpha = 0,lambda = grid,thresh = 1e-12)
-
-ridge.pred=predict(ridge.mod, s = 2, newx = x[test_set_indices,])
-
-mean((ridge.pred-y.test)^2)
+grid = 10^seq(10, -2, length = 100)
 
 
-ridge.pred=predict(ridge.mod,s=0,newx=x[test_set_indices,],exact=T)
-mean((ridge.pred-y.test)^2)
-
-
-#set.seed(159)
-cv.out=cv.glmnet(x[train_set_indices ,],y[train_set_indices],alpha =0)
-plot(cv.out)
-bestlam=cv.out$lambda.min
+ridge_reg = glmnet(x[train_set_indices,],y[train_set_indices], 
+	alpha = 0,lambda = grid)
+set.seed(159)
+ridge_cv = cv.glmnet(x[train_set_indices ,],y[train_set_indices], alpha =0, nfold = 10, lambda = grid, intercept =FALSE,standardize = FALSE)
+bestlam=ridge_cv$lambda.min
 bestlam
 
-ridge.pred=predict(ridge.mod,s=bestlam,newx=x[test_set_indices ,])
-mean((ridge.pred-y.test)^2)
+png(filename="images/ridge-validation.png")
+plot(ridge_cv)
+dev.off()
+
+
+ridge_pred=predict(ridge_reg,s=bestlam,newx=x[test_set_indices,])
+testMSE <- mean((ridge_pred-y.test)^2)
 
 
 out=glmnet(x,y,alpha=0)
-predict(out,type="coefficients",s=bestlam)
+ridge_final <- predict(out,type="coefficients",s=bestlam)
+
+
+# output primary results
+sink("data/ridge-results.txt")
+
+cat("Best Lambda:\n")
+bestlam
+cat("Official Coefficients:\n")
+ridge_final
+cat("Test MSE:\n")
+testMSE
+
+sink()
+
+save(ridge_pred, ridge_cv, bestlam,testMSE,ridge_final, file ="data/Ridge-Regression.RData")
 
