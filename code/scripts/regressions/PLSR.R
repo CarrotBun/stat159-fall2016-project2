7e750740cc23766bc19acea9a75892f5321d26cf
Lily Li <ylilyli24@gmail.com>
2016-10-25 22:40:02 -0700

    Regression: added full data predictions

--- code/scripts/regressions/PLSR.R
+++ code/scripts/regressions/PLSR.R
@@ -15,26 +15,33 @@ validation = "CV")
 
 #best model
 library(pander)
-mse_plsr = min(pls_reg$validation$PRESS)
-pls_reg$validation$PRESS
-# 7 comps
+bestmodel = which.min(pls_reg$validation$PRESS)
 
 # plot CV errors MSEP
-png("images/plsr-mse-plot.png")
+png("images/plsr-validation.png")
 validationplot(pls_reg, val.type = "MSEP", main = "PLSR Cross Validated Error")
 abline(v = 7, lty = 2)
 dev.off()
 
-# prediction plot
-test_pls_reg = plsr(as.vector(credit[test_set_indices, 12]) ~ as.matrix(credit[test_set_indices, 1:11]), 
-               ncomp = 7)
-MSEP(test_pls_reg)
-test_mse_plsr = 0.05045 
+###### prediction plot ################################################################### 
+plot(predict(pls_reg, as.matrix(credit[test_set_indices, 1:11]), ncomp = bestmodel), 
+     type = "l", col = "red",main = "PLSR Predicted and Actual Credit Balances", 
+     ylab = "Normalized Credit Balance")
+
+lines(credit[test_set_indices, 12], col = "black")
+
+legend(0, 3, legend = c("Predicted", "Actual"), fill = c("red", "black"), bty = "n")
+##########################################################################################
+
+pls_tMSE = mean((pls_pred - credit[test_set_indices, 12])^2) 
 
 # prediction on full data set
-plsr_pred = plsr(as.vector(credit[ ,12]) ~ as.matrix(credit[ ,1:11]),
-                      ncomp = 7)
+pls_final = plsr(as.vector(credit[ ,12]) ~ as.matrix(credit[ ,1:11]),
+                      ncomp = bestmodel)
 
 # save regressions
-save(pls_reg, test_pls_reg, plsr_pred,file = "data/pls-regression.RData")
+save(pls_reg, bestmodel, pls_tMSE, pls_final, file = "data/PLS-Regression.RData")
+
+
+
 
