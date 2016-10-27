a315e6bdd3885bc0cc15e0ffe3a650294e036d8d
Elly Wang <ellywjy@berkeley.edu>
2016-10-27 13:29:05 -0700

    OLS: saved output and objects into txt/RData file

--- code/scripts/regressions/OLS.R
+++ code/scripts/regressions/OLS.R
@@ -7,4 +7,9 @@ OLS_reg = lm(Balance ~., data = credit)
 
 # table summary
 library(pander)
+sink("data/ols-results.txt")
 pander(OLS_reg)
+sink()
+
+save(OLS_reg, file ="data/OLS-Regression.RData")
+
