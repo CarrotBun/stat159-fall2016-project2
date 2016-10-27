e252379a75f3b55965bc944412a65cb2c9c93256
Elly Wang <ellywjy@berkeley.edu>
2016-10-27 14:43:26 -0700

    OLS: added calculation of MSE

--- code/scripts/regressions/OLS.R
+++ code/scripts/regressions/OLS.R
@@ -1,15 +1,21 @@
 
 # import data
 credit = read.csv("data/scaled-credit.csv")
+source("code/functions/regression-functions.R")
 
 # OLS regression
 OLS_reg = lm(Balance ~., data = credit)
 
+OLS_tMSE = sqrt(residual_std_error(OLS_reg))
+
 # table summary
 library(pander)
 sink("data/ols-results.txt")
+cat("MSE:\n")
+OLS_tMSE
+cat("\n Official Coefficients:\n")
 pander(OLS_reg)
 sink()
 
-save(OLS_reg, file ="data/OLS-Regression.RData")
+save(OLS_reg, OLS_tMSE, file ="data/OLS-Regression.RData")
 
