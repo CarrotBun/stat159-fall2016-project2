a5c82357b8d7b6fbda4a3141ccf2c72a244c001a
Lily Li <ylilyli24@gmail.com>
2016-10-29 18:57:26 -0700

    Regression: added save data

--- code/scripts/regressions/Lasso.R
+++ code/scripts/regressions/Lasso.R
@@ -1,6 +1,9 @@
 # import csv
 credit = read.csv("data/scaled-credit.csv")
 
+# set seed
+set.seed(159)
+
 # lasso regression
 source("code/scripts/Train-Test.R")
 library(glmnet)
@@ -10,20 +13,16 @@ lasso_reg = cv.glmnet(as.matrix(credit[train_set_indices, 1:11]),
                       intercept = FALSE,
                       standardize = FALSE, 
                       lambda = grid)
+# min lambda
+best_lam = lasso_reg$lambda.min
 
-lasso_reg_min_lambda = lasso_reg$lambda.min
-
-png("images/lasso-lambda-plot.png")
-plot(lasso_reg$lambda, ylab = "Lasso Lambda", main = "Lambda Grid")
-abline(v = lasso_reg_min_lambda)
-dev.off()
-
-png("images/lasso-mse-plot.png")
+# plot CV errors MSEP
+png("images/lasso-validation.png")
 plot(lasso_reg)
 dev.off()
 
 
-# prediction plot
+# prediction plot ================================================
 png("images/lasso-prediction-plot.png")
 plot(predict(lasso_reg, as.matrix(credit[test_set_indices, 1:11]), s = "lambda.min"), type = "l"
      , col = "red",main = "Predicted and Actual Credit Balances", 
@@ -33,32 +32,28 @@ lines(credit[test_set_indices, 12], col = "black")
 
 legend(0, 3, legend = c("Predicted", "Actual"), fill = c("red", "black"), bty = "n")
 dev.off()
+# ================================================
+  
+
+lasso_pred = predict(lasso_reg, as.matrix(credit[test_set_indices, 1:11]), s = best_lam)
 
 # test MSE
-test_lasso_reg = cv.glmnet(as.matrix(credit[test_set_indices, 1:11]), 
-                           as.vector(credit[test_set_indices, 12]), 
-                           intercept = FALSE,
-                           standardize = FALSE, 
-                           lambda = grid)
-
-png("images/test-lasso-mse-plot.png")
-plot(test_lasso_reg)
-dev.off()
+lasso_tMSE = mean((lasso_pred - credit[test_set_indices, 12])^2) 
 
 # refit model on full data set
-lasso_pred_coef = predict(lasso_reg, as.matrix(credit[ , 1:11]), s = "lambda.min")
+lasso_final = predict(lasso_reg, as.matrix(credit[ , 1:11]), s = "lambda.min")
 
 # save to RData
-save(lasso_reg, test_lasso_reg, file = "data/lasso-regression.RData")
+save(lasso_reg, best_lam, lasso_tMSE, lasso_final, file = "data/Lasso-Regression.RData")
+
+library(pander)
 
 # save to textfile
-sink("data/lasso-output.txt")
-cat("lambda")
-lasso_reg_min_lambda
-cat("\n")
-cat("coefficients")
-cat("\n")
-lasso_pred_coef
+sink("data/lasso-results.txt")
+cat("\n best lamba:")
+best_lam
+cat("\n Lasso test MSE:")
+lasso_tMSE
 sink()
 
 
