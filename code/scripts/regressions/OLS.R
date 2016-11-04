67b51c9011ffc3bad8e71d6b2932a8b2cd22d14f
Elly Wang <ellywjy@berkeley.edu>
2016-11-04 16:56:57 -0700

    ols: added final regression on the full set

--- code/scripts/regressions/OLS.R
+++ code/scripts/regressions/OLS.R
@@ -10,14 +10,16 @@ OLS_reg = lm(Balance ~., data = credit[train_set_indices,])
 OLS_pred = predict(OLS_reg, credit[test_set_indices,])
 OLS_tMSE = mean((OLS_pred-y.test)^2)
 
+OLS_final = lm(Balance~., data = credit)
+
 # table summary
 library(pander)
 sink("data/ols-results.txt")
 cat("MSE:\n")
 OLS_tMSE
 cat("\n Official Coefficients:\n")
-pander(OLS_reg)
+pander(OLS_final)
 sink()
 
-save(OLS_reg, OLS_tMSE, file ="data/OLS-Regression.RData")
+save(OLS_reg, OLS_tMSE, OLS_final, file ="data/OLS-Regression.RData")
 
